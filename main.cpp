919ed98359222e2538bd60cdb527635c70a0adf2
dundat27 <dunlap16@gmail.com>
2019-07-28 19:23:19 -0700

    Added first set of code to main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	constexpr int WORD_LENGTH = 9;
+	cout << "Welcome to Bulls and Cows" << endl;
+	cout << "Can you guess the " << WORD_LENGTH;
+	cout << " letter iosgram that I'm thinking of?\n";
+	return 0;
+}
